cc91dbc0ed1c6430e851fc0b282d4f499222c41b
Philip Hua <philiphua@88E9FE649BAE.local>
2021-02-28 17:55:16 -0800

    syntax error

--- selfdrive/version.py
+++ selfdrive/version.py
@@ -49,7 +49,7 @@ terms_version: bytes = b"2"
 
 dirty: bool = True
 comma_remote: bool = False
-fastzeek_remote bool = False
+fastzeek_remote: bool = False
 tested_branch: bool = False
 origin = get_git_remote()
 branch = get_git_full_branchname()
